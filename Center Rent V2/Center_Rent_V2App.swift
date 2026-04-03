5202c82d98b8e661ad75a01b724f0a18082a9874
Giovanni <giovanni.antony@icloud.com>
2026-04-03 18:48:12 -0300

    Initial Commit

---
+++ Center Rent V2/Center_Rent_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Center_Rent_V2App.swift
+//  Center Rent V2
+//
+//  Created by Giovanni Antony on 03/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Center_Rent_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Center Rent V2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Center Rent V2
+//
+//  Created by Giovanni Antony on 03/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
